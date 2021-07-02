242057f03471922526eb7871efc9655b56a99121
Xu Feng Cheng <benny12628@gmail.com>
2021-07-02 22:46:57 +0800

    feat: 使用 winston 紀錄日誌

---
+++ src/utils/logConfiguration.ts
@@ -0,0 +1,23 @@
+import { format, transports } from 'winston';
+
+const { combine, timestamp, printf /* prettyPrint */ } = format;
+
+const myFormat = printf(
+  ({ level, message, timestamp: formatTimestamp }) => `${level}: [${formatTimestamp}] ${message}`,
+);
+
+const logConfiguration = {
+  format: combine(timestamp(), myFormat),
+  level: 'info',
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new transports.Console(),
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' }),
+  ],
+};
+
+export default logConfiguration;
